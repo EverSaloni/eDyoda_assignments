fe1d2492d83a1e57143efa7929d75fd07267f299
EverSaloni <125963002+EverSaloni@users.noreply.github.com>
2023-03-26 14:27:21 +0530

    Add files via upload

---
+++ complete_student_class.py
@@ -0,0 +1,12 @@
+class Student:
+    def setName(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
+
+    def setRollNumber(self, rollNumber):
+        self.__rollNumber = rollNumber
+
+    def getRollNumber(self):
+        return self.__rollNumber
